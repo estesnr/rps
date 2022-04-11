9390bcce8bb010dd1c6c4819029835a394660e53
Nate Estes <97855282+estesnr@users.noreply.github.com>
2022-04-11 14:37:34 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
-document.querySelector("button").addEventListener("click", playGame)
+// document.querySelector("button").addEventListener("click", playGame)
+document.getElementById('rockIcon').addEventListener('click', useRock)
+document.getElementById("paperIcon").addEventListener('click', usePaper)
+document.getElementById('scissorIcon').addEventListener("click", useScissors)
 
+// initial scorecards
+let playScore = 0
+let compScore = 0
 
-function playGame() {
+function playGame(userInput) {
     
-    let userInput = document.querySelector('input').value.toLowerCase();
     let randNum = Math.random();
     let pcInput = ""
+    let playerScore = document.getElementById('player-score')
+    let pcScore = document.getElementById('pc-score')
 
     if(randNum < .33) {
         pcInput = "rock"
@@ -17,13 +24,31 @@ function playGame() {
         pcInput = "scissors"
     }
 
-    if((userInput == "rock" && pcInput == "scissors") || (userInput == "scissors" && pcInput == "paper") || (userInput == "paper" && pcInput == "rock")) {
+    if((userInput == "rock" && pcInput == "scissors") || (userInput == "scissors" && pcInput == "paper") ||
+    (userInput == "paper" && pcInput == "rock")) {
         alert(`YOU CHOSE ${userInput}, OPPONENT CHOSE ${pcInput}, YOU WIN!!`)
+        playScore += 1
+        playerScore.innerHTML = playScore
     }
     else if(userInput == pcInput) {
         alert(`YOU CHOSE ${userInput}, OPPONENT CHOSE ${pcInput}, DRAW!!`)
     }
     else {
         alert(`YOU CHOSE ${userInput}, OPPONENT CHOSE ${pcInput}, YOU LOSE!!`)
+        compScore += 1
+        pcScore.innerHTML = compScore
     }
 }
+
+function useRock() {
+    playGame("rock")
+}
+
+function usePaper() {
+    playGame("paper")
+}
+
+function useScissors() {
+    playGame("scissors")
+}
+
